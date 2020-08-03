7a15c135e88486b2076fa0541faeaa19ad72a606
shreedhar desai <desaishreedhar09@gmail.com>
2020-08-03 13:23:15 +0530

    minor changes

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { NativeSelect, FormControl } from '@material-ui/core';
+//import { NativeSelect, FormControl } from '@material-ui/core';
 import Card from '../Cards/Cards';
 import { fetchCountries } from '../../api';
 
